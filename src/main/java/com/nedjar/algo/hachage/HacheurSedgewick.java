cf42239362b12fdaa698e93f532f02fea7b0a6f0
Sébastien NEDJAR <nedseb@goldaire.com>
2012-10-02 17:18:39 +0200

    Ajout de la classe HacheurSedgewick

---
+++ src/main/java/com/nedjar/algo/hachage/HacheurSedgewick.java
@@ -0,0 +1,16 @@
+package com.nedjar.algo.hachage;
+
+import com.nedjar.dataStructure.Tuple;
+
+public class HacheurSedgewick implements Hacheur {
+
+	@Override
+	public long hache(Tuple key) {
+		int h = 0;
+		int a = 127;
+		for (byte i = 0; i < key.size(); i++) {
+			h=(a*h + key.get(i));
+		}
+		return h;
+	}
+}
