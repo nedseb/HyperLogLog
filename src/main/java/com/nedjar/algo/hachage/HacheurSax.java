81076dc0e509319f80dee15c975793e2be80ef81
Sébastien NEDJAR <nedseb@goldaire.com>
2012-10-02 17:18:18 +0200

    Ajout de la classe HacheurSax

---
+++ src/main/java/com/nedjar/algo/hachage/HacheurSax.java
@@ -0,0 +1,16 @@
+package com.nedjar.algo.hachage;
+
+import com.nedjar.dataStructure.Tuple;
+
+public class HacheurSax implements Hacheur {
+
+	@Override
+	public long hache(Tuple key) {
+		int h = 0;
+		for (byte i = 0; i < key.size(); i++) {
+			h^=(h<<5) + (h>>2) +  key.get(i);
+		}
+		return h;
+	}
+
+}
