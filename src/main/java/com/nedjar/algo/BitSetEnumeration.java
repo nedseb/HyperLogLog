1c864b1520c1af159852c94b9f2516afb5dd8543
Sébastien NEDJAR <nedseb@goldaire.com>
2012-10-03 17:16:07 +0200

    Transformation de la classe BitSetEnumeration en un énumérateur

--- src/main/java/com/nedjar/algo/BitSetEnumeration.java
+++ src/main/java/com/nedjar/algo/BitSetEnumeration.java
@@ -1,25 +1,51 @@
 package com.nedjar.algo;
+
 import java.util.BitSet;
+import java.util.Enumeration;
 
+public class BitSetEnumeration implements Enumeration<BitSet> {
+	private BitSet	b;
+	private int nbits;
 
+	public BitSetEnumeration(int nbits) {
+		super();
+		this.b = new BitSet(nbits);
+		this.nbits = nbits;
+	}
 
-public class BitSetEnumeration {
-	public static void next(BitSet b)
-	{
-		int i; 
-		for(i=0; i< b.size();i++){
-			b.set(i,!b.get(i));
-			if(b.get(i)){
+	@Override
+	public boolean hasMoreElements() {
+		return b.cardinality() < nbits;
+	}
+
+	@Override
+	public BitSet nextElement() {
+		int i;
+		for (i = 0; i < b.size(); i++) {
+			b.set(i, !b.get(i));
+			if (b.get(i)) {
+				return b;
+			}
+		}
+		b.set(i);
+		return b;
+	}
+
+	public static void next(BitSet b) {
+		int i;
+		for (i = 0; i < b.size(); i++) {
+			b.set(i, !b.get(i));
+			if (b.get(i)) {
 				return;
 			}
 		}
 		b.set(i);
 	}
-	
-	public static void main(String[] args) 
-	{
-		for (BitSet b = new BitSet(10); b.cardinality() <= 10;next(b)) {
-			System.out.println(b);
-		}
+
+	public static void main(String[] args) {
+		BitSetEnumeration bitSetEnum = new BitSetEnumeration(5);
+		for (Enumeration<BitSet> e = bitSetEnum; e.hasMoreElements();)
+			System.out.println(e.nextElement());
+
 	}
 }
