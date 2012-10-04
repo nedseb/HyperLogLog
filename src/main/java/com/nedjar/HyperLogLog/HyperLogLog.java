a366f5a95fb4e7dd804578e8ea97ce976e503139
Sébastien NEDJAR <nedseb@goldaire.com>
2012-10-04 22:46:40 +0200

    ebauche de l'algo HyperLogLog

--- src/main/java/com/nedjar/HyperLogLog/HyperLogLog.java
+++
@@ -1,5 +0,0 @@
-package com.nedjar.HyperLogLog;
-
-public class HyperLogLog {
-
-}
--- src/main/java/com/nedjar/estimateurs/HyperLogLog.java
+++ src/main/java/com/nedjar/estimateurs/HyperLogLog.java
@@ -1,5 +1,31 @@
 package com.nedjar.estimateurs;
 
-public class HyperLogLog {
+import com.nedjar.algo.hachage.Hacheur;
+import com.nedjar.dataStructure.Tuple;
 
+public class HyperLogLog<T extends Iterable<Tuple>> implements Estimateur<T> {
+	
+	private Hacheur hacheur;
+	private int b;
+	private int m;
+	private long[] registers;
+	
+	public HyperLogLog(Hacheur hacheur, int b) {
+		super();
+		this.hacheur = hacheur;
+		this.b = b;
+		this.m = 2^b;
+		this.registers = new long[m];
+	}
+
+
+
+	public int estimerCardinalite(T dataSet) {	
+		int res = 0;
+		for (Tuple tupleCour : dataSet) {
+			long hashCour = hacheur.hache(tupleCour);
+			long index = hashCour >> (Long.SIZE - b);
+		}
+		return res;
+	}
 }
